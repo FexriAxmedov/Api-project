d80b3c139e100899c430576da8e80e8f0f2407de
{name} <{emailaddress}>
2023-01-06 23:04:54 +0400

    second commit

--- HospitalManagement/Startup.cs
+++ HospitalManagement/Startup.cs
@@ -1,7 +1,9 @@
+using HospitalDAL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,11 @@ namespace HospitalManagement
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDBContext>(opts =>
+            {
+                opts.UseSqlServer(Configuration.GetConnectionString("cString"));
+            });
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
